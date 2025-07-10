13aec9a66f927017f909120481893643a54dba44
Tikesh <aswaletinku@gmail.com>
2025-07-10 17:16:05 +0530

    Fix Cors issue

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://candidatereferal.netlify.app/"],
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
