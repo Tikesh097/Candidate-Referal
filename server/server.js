7eb1cf6ea993b6233439af1e3acf5160e51a3e79
Tikesh <aswaletinku@gmail.com>
2025-07-10 19:32:03 +0530

    Fix Cors issue final 2

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",                //local dev frontend
-    "https://candidatereferal.netlify.app"  //deployed frontend
+    "http://localhost:5173",                //ISSUE FIX 
+    "https://candidatereferal.netlify.app"  
   ],
   credentials: true,
 }));
